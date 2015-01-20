e6aa6792ef13cf452b9c9c757681904ded4d69a6
Anh <hoanh04@yahoo.com>
2015-01-20 08:32:28 +0700

    prz2

--- module/Candidate/src/Candidate/Model/prApiCoreCandidateClass.php
+++ module/Candidate/src/Candidate/Model/prApiCoreCandidateClass.php
@@ -141,5 +141,246 @@ class prApiCoreCandidateClass extends AbstractTableGateway
 
         return $records;
     }
+
+    public function get_attribute_rubrics($TemplateID)
+    {
+        $db = new Sql($this->adapter);
+        $select = $db->select();
+        $select->from(array('att'=>'attribute_rubrics'));
+        $select->where(array("att.TemplateID" => $TemplateID));
+        $statement = $db->prepareStatementForSqlObject($select);
+        $results = $statement->execute();
+
+        $records = array();
+        foreach ($results as $result) {
+            $records[] = $result;
+        }
+        if(empty($records) && count($records)==0){
+            return array();
+        } else {
+            return $records;
+        }
+    }
+
+    public function get_attribute_child($cadidate_profileID, $ID)
+    {
+        $db = new Sql($this->adapter);
+        $select = $db->select();
+        $select->from(array('attr'=>'attribute'));
+        $select->join(array('ct'=>'attribute_category'),
+            'attr.AtttributeCategoryID = ct.AttributeCatetoryID',
+            array('AttributeCategory'),
+            $select::JOIN_LEFT
+        );
+        $select->join(array('cav'=>'candidate_attribute_value'),
+            new Expression('attr.ID = cav.AttributeID AND cav.Candidate_ProfileID ="'.$cadidate_profileID.'"'),
+            array('YearsofExperience','LevelofInterest','Candidate_ProfileID','Value'),
+            $select::JOIN_LEFT
+        );
+        $select->where(array("attr.ParentAttributeID" =>$ID));
+
+        $statement = $db->prepareStatementForSqlObject($select);
+        $results = $statement->execute();
+
+        $records = array();
+        foreach ($results as $result) {
+            $records[] = $result;
+        }
+
+        if(empty($records) && count($records)==0){
+            return array();
+        } else {
+            $list = array();
+            foreach($records as $rec){
+                $rec['TemplateID'] = $this->get_attribute_rubrics($rec['AttributeRubricTemplateID']);
+
+                $list[] = $rec;
+            }
+            return $list;
+        }
+    }
+
+    public function get_attribute_parent($ID,$ParentAttributeID,$AtttributeCategoryID)
+    {
+        $db = new Sql($this->adapter);
+        $select = $db->select();
+        $select->columns(array('*'));
+        $select->from(array('attr'=>'attribute'));
+        $select->join(array('ct'=>'attribute_category'),
+            'attr.AtttributeCategoryID = ct.AttributeCatetoryID',
+            array('AttributeCategory'),
+            $select::JOIN_LEFT
+
+        );
+
+        $select->join(array('cav'=>'candidate_attribute_value'),
+            new Expression('attr.ID = cav.AttributeID AND cav.Candidate_ProfileID ="'.$ID.'"'),
+            array('YearsofExperience','LevelofInterest','Candidate_ProfileID','Value'),
+            $select::JOIN_LEFT
+        );
+
+        $select->where("attr.ParentAttributeID = '".$ParentAttributeID."' && attr.AtttributeCategoryID = '".$AtttributeCategoryID."' " );
+        $select->where(array("attr.ParentAttributeID" =>$ParentAttributeID, "attr.AtttributeCategoryID" => $AtttributeCategoryID));
+
+        $select->order(array('attr.AtttributeCategoryID ASC','attr.ID ASC' ));
+
+        $statement = $db->prepareStatementForSqlObject($select);
+        $results = $statement->execute();
+        $records = array();
+        foreach ($results as $result) {
+            $records[] = $result;
+        }
+
+        if(empty($records) && count($records)==0){
+            return array();
+        } else {
+            $list = array();
+            foreach($records as $rec){
+                $rec['TemplateID'] = $this->get_attribute_rubrics($rec['AttributeRubricTemplateID']);
+
+                $list[] = $rec;
+            }
+            return $list;
+        }
+    }
+
+    public function updateCandidateAttribute($CandidateProfileID,$attrs){
+        $db = new Sql($this->adapter);
+        $select = $db->select();
+        $select->from(array('cav'=>'candidate_attribute_value') );
+
+        $select->where(array("cav.Candidate_ProfileID" =>$CandidateProfileID));
+        $statement = $db->prepareStatementForSqlObject($select);
+        $results = $statement->execute();
+        $records = array();
+        foreach ($results as $result) {
+            $records[] = $result;
+        }
+
+        if(count($records)>0){
+            if(empty($attrs) || count($attrs)==0){
+                $select = $db->delete();
+                $select->from('candidate_attribute_value');
+                $select->where(array("Candidate_ProfileID" =>$CandidateProfileID));
+                $statement = $db->prepareStatementForSqlObject($select);
+                $results = $statement->execute();
+
+            } else{
+
+                foreach($records as $rec){
+                    $database_Exist=$rec['AttributeID'];
+                    $flag_exsist = false;
+                    foreach($attrs as $attrInfo){
+                        if($database_Exist == $attrInfo['attr_id']){
+                            $flag_exsist = true;
+                            break;
+                        }
+                    }
+
+                    if(!$flag_exsist){
+                        $select = $db->delete();
+                        $select->from('candidate_attribute_value');
+                        $select->where(array("Candidate_ProfileID" =>$CandidateProfileID, "AttributeID" => $database_Exist));
+                        $statement = $db->prepareStatementForSqlObject($select);
+                        $results = $statement->execute();
+                    }
+                }
+
+                // if $attr not exist in database then insert, else update
+                foreach($attrs as $attributeInfo){
+                    $attr_Exist=$attributeInfo['attr_id'];
+                    $flag_exsist = false;
+                    foreach($records as $recordsInfo){
+                        if($attr_Exist == $recordsInfo['AttributeID']){
+                            $data = array('Candidate_ProfileID'=>$CandidateProfileID,'AttributeID'=>$attributeInfo['attr_id'],'Value'=>$attributeInfo['attr_value'],'YearsofExperience'=>$attributeInfo['attr_YoE'],'LevelofInterest'=>$attributeInfo['attr_LevelofInterest']);
+                            $select = $db->update();
+                            $select->table('candidate_attribute_value');
+                            $select->set($data);
+                            $select->where(array("Candidate_ProfileID" => $CandidateProfileID, "AttributeID" => $attr_Exist));
+                            $statement = $db->prepareStatementForSqlObject($select);
+                            $results = $statement->execute();
+                            $flag_exsist = true;
+                            break;
+                        }
+                    }
+
+                    if(!$flag_exsist){
+                        $data=array('Candidate_ProfileID'=>$CandidateProfileID,'AttributeID'=>$attributeInfo['attr_id'],'Value'=>$attributeInfo['attr_value'],'YearsofExperience'=>$attributeInfo['attr_YoE'],'LevelofInterest'=>$attributeInfo['attr_LevelofInterest']);
+                        $insert = $db->insert('candidate_attribute_value');
+                        $insert->values($data);
+                        $statement = $db->prepareStatementForSqlObject($insert);
+                        $affectedRows = $statement->execute()->getAffectedRows();
+                    }
+                }
+
+            }
+        } else {
+            if(!empty($attrs) && count($attrs)>0){
+                foreach($attrs as $id){
+                    $data=array('Candidate_ProfileID'=>$CandidateProfileID,'AttributeID'=>$id['attr_id'],'Value'=>$id['attr_value'],'YearsofExperience'=>$id['attr_YoE'],'LevelofInterest'=>$id['attr_LevelofInterest']);
+                    $insert = $db->insert('candidate_attribute_value');
+                    $insert->values($data);
+                    $statement = $db->prepareStatementForSqlObject($insert);
+                    $affectedRows = $statement->execute()->getAffectedRows();
+                }
+            }
+        }
+        return true;
+    }
+
+    public function saveContactInfo($userID, $data)
+    {
+        $avaiFields = array('firstname','middlename','lastname','dob','emailaddress',
+            'URL','PhoneNumber','Address1','Address2','City','State','PostalCode','Country','faxnumber','URlnetwork','instanmessaing');
+        $contactInfo = $this->getContactInfo($userID);
+
+        $updateFields = array();
+        foreach($data as $k=>$v){
+            if(in_array($k,$avaiFields)){
+                if($v != $contactInfo[$k]){
+                    $updateFields[$k] = $v;
+                }
+            }
+        }
+
+        if(count($updateFields)==0){
+            return 0;
+        } else {
+            $db = new Sql($this->adapter);
+            $select = $db->update();
+            $select->table('user');
+            $select->set($updateFields);
+            $select->where(array("UserID"=> $userID));
+            $statement = $db->prepareStatementForSqlObject($select);
+            $results = $statement->execute()->getAffectedRows();
+
+        }
+    }
+
+    public function getListCandidatePortfolio($userID)
+    {
+
+        $db = new Sql($this->adapter);
+        $select = $db->select();
+        $select->from(array('capfl'=>'candidate_portfolio'),
+            array('CandidatePortfolioID','CandidateProfileID','Title','URL','Description','IconURL')
+        );
+        $select->join(array('u'=>'user'),
+            'u.CandidateProfileID = capfl.CandidateProfileID',
+            array('UserID'),
+            $select::JOIN_INNER
+        );
+        $select->where(array("u.UserID" =>$userID));
+        $select->where(array("u.usertype" => 2));
+        $statement = $db->prepareStatementForSqlObject($select);
+        $results = $statement->execute();
+
+        $records = array();
+        foreach ($results as $result) {
+            $records[] = $result;
+        }
+        return $records;
+    }
+
 }
 ?>
